6a6ef339bbd40311db5bb01b64130eda3e08755b
yoouyeon <jyeon.yoon59@gmail.com>
2023-04-07 16:47:53 +0900

    [Style] default layout body style 적용

--- src/layouts/DefaultLayout.jsx
+++ src/layouts/DefaultLayout.jsx
@@ -1,12 +1,15 @@
 import { Outlet } from 'react-router-dom';
 import { Header, Sidebar } from '../components/layouts';
+import styles from '../styles/content.module.scss';
 
 export function DefaultLayout() {
   return (
-    <>
+    <div className={styles.generalWrapper}>
       <Header />
       <Sidebar />
-      <Outlet />
-    </>
+      <div className={styles.bodyWrapper}>
+        <Outlet />
+      </div>
+    </div>
   );
 }
