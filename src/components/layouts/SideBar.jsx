02ff1b7da1cda53cb8b58e9bd9696eb792606655
yoouyeon <jyeon.yoon59@gmail.com>
2023-04-10 13:51:57 +0900

    [Style] sidebar style 추가

--- src/components/layouts/SideBar.jsx
+++ src/components/layouts/SideBar.jsx
@@ -2,20 +2,30 @@ import { Link } from 'react-router-dom';
 import { sidebarContent } from '../../commons/sidebarContent';
 import { useRecoilValue } from 'recoil';
 import { userState } from '../../utils/user';
+import { useRoute } from '../../utils/useRoute';
+import styles from '../../styles/sidebar.module.scss';
 
 export function Sidebar() {
   const user = useRecoilValue(userState);
+  const { currentPath } = useRoute();
   if (!user) {
     return <></>;
   }
   return (
-    <nav>
-      <ul>
+    <nav className={styles.wrapper}>
+      <ul className={styles.ulWrapper}>
         {sidebarContent
           .filter((content) => content.auth <= user.role)
           .map((route) => (
             <li key={route.name}>
-              <Link to={route.path}>{route.name}</Link>
+              <Link
+                className={
+                  route.path === currentPath ? styles.currentLink : styles.link
+                }
+                to={route.path}
+              >
+                {route.name}
+              </Link>
             </li>
           ))}
       </ul>
