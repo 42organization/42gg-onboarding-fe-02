0bd32845f8a030c9a0c754ede6b0410125182aa6
yoouyeon <jyeon.yoon59@gmail.com>
2023-04-10 14:00:25 +0900

    [Feat] Logout 기능 추가

--- src/components/layouts/Header.jsx
+++ src/components/layouts/Header.jsx
@@ -1,9 +1,24 @@
 import styles from '../../styles/header.module.scss';
+import { useRoute } from '../../utils/useRoute';
+import { logout } from '../../utils/login';
+import { useResetRecoilState } from 'recoil';
+import { userState } from '../../utils/user';
 export function Header() {
+  const { router } = useRoute();
+  const resetUser = useResetRecoilState(userState);
+
+  function handleLogout() {
+    logout();
+    resetUser();
+    router('/login');
+  }
+
   return (
     <header className={styles.wrapper}>
       <div className={styles.title}>Routing Practice</div>
-      <button className={styles.button}>Logout</button>
+      <button onClick={handleLogout} className={styles.button}>
+        Logout
+      </button>
     </header>
   );
 }
