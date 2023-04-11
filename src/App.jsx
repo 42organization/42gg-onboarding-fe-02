a02690c4f2d052866fef6edb8278267fba2b1591
yoouyeon <jyeon.yoon59@gmail.com>
2023-04-11 17:16:05 +0900

    [Fix] unhandled request warning message 제거

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,9 @@ import { worker } from './mocks/browser';
 import { Toaster } from 'react-hot-toast';
 
 if (process.env.NODE_ENV === 'development') {
-  worker.start();
+  worker.start({
+    onUnhandledRequest: 'bypass',
+  });
 }
 
 function App() {
