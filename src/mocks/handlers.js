e7dfd6707a8fd229ad22ad103f04965cd53490cc
yoouyeon <jyeon.yoon59@gmail.com>
2023-04-17 12:15:49 +0900

    [Chore] 불필요한 파일, 주석 삭제

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -1,5 +1,4 @@
 import { rest } from 'msw';
-import { v1 } from 'uuid';
 import { userList } from '../commons';
 
 export const handlers = [
@@ -15,14 +14,6 @@ export const handlers = [
     if (user.password !== password) {
       return res(ctx.status(401), ctx.json({ msg: '잘못된 비밀번호입니다.' }));
     }
-    // const token = `${username}-${v1()}`; // 토큰 생성
-    // user.token = token; // userList에 토큰 저장
-    /**
-     * TODO: 토큰을 매 로그인시마다 새로 생성하고, 로그아웃시에 삭제할 수 있는 방법을 고민해보기
-     * - 지금은 이미 저장되어 있는 하나의 토큰을 계속 사용하고 있음.
-     * - js파일 내 객체에 저장해두면 refresh시에 초기화됨.
-     * - 토큰을 localStorage에 저장하면 refresh시에도 유지되지만, 보안상의 문제가 있음.
-     */
     return res(
       ctx.status(200),
       ctx.cookie('auth-token', user.token),
