8fe6fb0483e7f840e30368f643c4217b645900a6
Hyobin Cho <hyobicho@c1r6s7.42seoul.kr>
2023-07-12 22:17:07 +0900

    [Styele] css 추가 및 컴포넌트 분리

--- gg02/src/App.js
+++ gg02/src/App.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import './css/App.css';
 import './css/styles.scss';
-import UserInput from './components/UserInput';
-import HandleButton from './components/HandleButton';
+import LoginForm from './components/LoginForm';
 
 function App() {
   return (
     <div className="App">
       <h1>HELLO WORLD </h1>
-      <UserInput />
-      <HandleButton />
+      <LoginForm />
     </div>
   );
 }
