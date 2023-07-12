41082ea2441803c57ed5242ea57c0f1693356014
Hyobin Cho <hyobicho@c1r6s7.42seoul.kr>
2023-07-12 16:12:45 +0900

    [Style] 컴포넌트 분리

--- gg02/src/App.js
+++ gg02/src/App.js
@@ -1,19 +1,15 @@
 import React from 'react';
 import './css/App.css';
 import './css/styles.scss';
+import UserInput from './components/UserInput';
+import HandleButton from './components/HandleButton';
 
 function App() {
   return (
     <div className="App">
       <h1>HELLO WORLD </h1>
-
-      <div className="Input">
-        <input name="username" type="text" placeholder='username' />
-        <input name="password" type="password" placeholder='password'/>
-      </div>
-      <div>
-        <button type="button">SIGN IN</button>
-      </div>
+      <UserInput />
+      <HandleButton />
     </div>
   );
 }
---
+++ gg02/src/components/UserInput.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function UserInput() {
+  return (
+    <div className="Input">
+      <input name="username" type="text" placeholder="username" />
+      <input name="password" type="password" placeholder="password" />
+    </div>
+  );
+}
+
+export default UserInput;
