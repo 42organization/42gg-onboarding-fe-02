979153bcf685ae34cc6ae6ffbac176240fd72e36
hyobin <hyobb109@gmail.com>
2023-07-13 03:05:27 +0900

    [Feat] 로그인 정보 추가

---
+++ gg02/src/UserInfo.js
@@ -0,0 +1,19 @@
+const users = [
+  {
+    id: 'hyobicho',
+    pw: '1234',
+    auth: 'normal',
+  },
+  {
+    id: 'manager1',
+    pw: '1234',
+    auth: 'manager',
+  },
+  {
+    id: 'admin1',
+    pw: '1234',
+    auth: 'admin',
+  },
+];
+
+export default users;
