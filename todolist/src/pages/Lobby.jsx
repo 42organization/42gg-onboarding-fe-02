5fd51e692fdecfa380f5d4a0b224783fc3414108
jaeywon <dnsjwodyd@gmail.com>
2024-01-30 19:40:35 +0900

    [feat] navbar css까지 적용완료 logout기능 구현 남음.
    1. css적용완료

--- todolist/src/pages/Lobby.jsx
+++ todolist/src/pages/Lobby.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import styles from '../styles/default.module.css';
+import Navbar from '../component/navbar';
 
 function Lobby() {
-  return <div></div>;
+  return (
+    <div className={styles.backgroundcolor}>
+      <Navbar />
+    </div>
+  );
 }
 
 export default Lobby;
