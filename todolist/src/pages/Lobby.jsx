f44d88c17e0115d3460180a119dc5379410653d1
jaeywon <dnsjwodyd@gmail.com>
2024-01-31 18:47:12 +0900

    [feat] lobby에 sidbar추가완료
    1.  lobby sidebar추가

--- todolist/src/pages/Lobby.jsx
+++ todolist/src/pages/Lobby.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import styles from '../styles/default.module.css';
 import Navbar from '../component/navbar';
+import Sidebar from '../component/sidebar';
 
 function Lobby() {
   return (
-    <div className={styles.backgroundcolor}>
+    <div>
       <Navbar />
+      <Sidebar />
     </div>
   );
 }
