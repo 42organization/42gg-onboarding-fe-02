12fff6f1f05587c31854cefff84ef236fc02a415
juha <contemplation.person@gmail.com>
2024-02-02 00:03:35 +0900

    feat: set react router

--- 42gg/src/App.js
+++ 42gg/src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import './App.css';
 import HomePage from './pages/HomePage.jsx';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <HomePage />,
+    errorElement: <h1> fail</h1>, // TODO: get error page
+  },
+]);
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <RouterProvider router={router} />
     </div>
   );
 }
