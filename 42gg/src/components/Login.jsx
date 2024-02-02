3754250b791fc66fa2b4ae8dda742a54e568345d
juha <contemplation.person@gmail.com>
2024-02-02 18:37:58 +0900

    feat: 로그인 로직 완성

--- 42gg/src/components/Login.jsx
+++ 42gg/src/components/Login.jsx
@@ -1,4 +1,6 @@
 import axios from 'axios';
+import React from 'react';
+import '../css/Login.css';
 import { BrowserRouter } from 'react-router-dom';
 
 export default Login;
@@ -8,17 +10,26 @@ function Login() {
     e.preventDefault();
     let id = e.target[0].value;
     let password = e.target[1].value;
-    axios.post('/id', { test: 'test' }).then((res) => {
-      console.log(res);
-    });
+    // Todo: fetch 사용할 것
+    axios
+      .post('/id', { id, password })
+      .then(({ data }) => {
+        if (data.id !== id) {
+          throw new Error('비정상 접근입니다.');
+        }
+        console.log('data', data.id);
+      })
+      .catch((err) => {
+        console.log('아이디 패스워드를 확인하세요.');
+      });
   }
 
   return (
-    <div>
-      <h1>ID</h1>
-      <h1>Password</h1>
+    <div className='Login'>
       <form onSubmit={handleSubmit}>
+        <h1>ID</h1>
         <input type='text' placeholder='ID' />
+        <h1>Password</h1>
         <input type='password' placeholder='Password' />
         <button type='submit'>Login</button>
       </form>
