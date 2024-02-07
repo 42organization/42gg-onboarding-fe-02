302dc3338cf6f7671d07f2544862857fa54c9ef5
conteng <contemplation.person@gmail.com>
2024-02-07 22:06:52 +0900

    feat: 핫한 라우팅 방식

--- 42gg/src/App.js
+++ 42gg/src/App.js
@@ -1,35 +1,24 @@
-import React from 'react';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { BrowserRouter, Route, Router, Routes } from 'react-router-dom';
 import './App.css';
 import HomePage from './app/pages/HomePage';
 import ErrorPage from './app/pages/ErrorPage';
 import AdminPage from './app/pages/AdminPage';
-import TodoPage from './app/pages/TodoPage';
+import ManagerPage from './app/pages/ManagerPage';
+import NormalPage from './app/pages/NormalPage';
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <HomePage />,
-    errorElement: <ErrorPage />,
-    children: [
-      {
-        path: '/adminPage',
-        element: <AdminPage />,
-        errorElement: <ErrorPage />,
-      },
-      {
-        path: '/todoPage',
-        element: <TodoPage />,
-        errorElement: <ErrorPage />,
-      },
-    ],
-  },
-]);
-
+// /normal/:id
 function App() {
   return (
     <div>
-      <RouterProvider router={router} />
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+          <Route path='/normal' element={<NormalPage />} />
+          <Route path='/manager' element={<ManagerPage />} />
+          <Route path='/admin' element={<AdminPage />} />
+          <Route path='/*' element={<ErrorPage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
