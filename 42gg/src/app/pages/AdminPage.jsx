ad92dbdaa764cda9eb6457d2b7f5f1b5c3ba262f
conteng <contemplation.person@gmail.com>
2024-02-07 22:11:04 +0900

    feat: useEffect 사용하여 잘못된 라우팅 홈으로 변경

--- 42gg/src/app/pages/AdminPage.jsx
+++ 42gg/src/app/pages/AdminPage.jsx
@@ -1,7 +1,16 @@
 import Navbar from '../components/NavBar';
 import Sidebar from '../components/Sidebar';
+import { userState } from '../../atom';
+import { useNavigate } from 'react-router-dom';
+import { useRecoilValue } from 'recoil';
+import { useEffect } from 'react';
 
 function AdminPage() {
+  const { role } = useRecoilValue(userState);
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (role !== 'admin') navigate('/');
+  }, []);
   return (
     <div className='AdminPage'>
       <Navbar />
