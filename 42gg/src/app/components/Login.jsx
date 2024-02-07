3dca3565587ba572e073e69355e8f6eb71b12791
conteng <contemplation.person@gmail.com>
2024-02-07 22:07:59 +0900

    feat: restfull api

--- 42gg/src/app/components/Login.jsx
+++ 42gg/src/app/components/Login.jsx
@@ -1,22 +1,28 @@
 import axios from 'axios';
 import React from 'react';
 import '../css/Login.css';
+import { useNavigate } from 'react-router-dom';
+import { useRecoilState } from 'recoil';
+import { userState } from '../../atom';
 
 export default Login;
 
 function Login() {
+  const [user, setUser] = useRecoilState(userState);
+  const navigate = useNavigate();
+  console.log(user);
+  if (user.role !== '') navigate('/' + user.role);
+
   function handleSubmit(e) {
     e.preventDefault();
     let id = e.target[0].value;
     let password = e.target[1].value;
     // Todo: fetch 사용할 것
     axios
-      .post('/id', { id, password })
+      .post('/login-id', { id, password })
       .then(({ data }) => {
-        if ('normal' === data.role) console.log('normal user');
-        else if ('manager' === data.role) console.log('good manager');
-        else if ('admin' === data.role) console.log('good admin');
-        else throw new Error('아이디 또는 비밀번호가 틀렸습니다.');
+        setUser({ id: data.id, role: data.role });
+        console.log('submit: ', user);
       })
       .catch((err) => {
         console.log(err);
