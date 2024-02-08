f8bda8b318e5f59db16091d5cc477c203bae9a9e
conteng <contemplation.person@gmail.com>
2024-02-08 22:48:50 +0900

    style: 간단하게 만들기

--- 42gg/src/app/components/LogoutButton.jsx
+++ 42gg/src/app/components/LogoutButton.jsx
@@ -1,14 +1,12 @@
 import { useResetRecoilState } from 'recoil';
 import { userState } from '../../atom';
-import { useNavigate } from 'react-router-dom';
 
 export default function LogoutButton() {
-  const navigate = useNavigate();
   const userReset = useResetRecoilState(userState);
 
-  function logoutHandler() {
-    userReset();
-    navigate('/');
-  }
-  return <button onClick={logoutHandler}>logout</button>;
+  return (
+    <button className='LogoutButton' onClick={userReset}>
+      logout
+    </button>
+  );
 }
