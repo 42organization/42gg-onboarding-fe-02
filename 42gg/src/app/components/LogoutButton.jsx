9f92fffe5265079ea7ec765bfa5f52a2e62592d6
conteng <contemplation.person@gmail.com>
2024-02-10 17:45:37 +0900

    feat: Logout button  에서 user reset

--- 42gg/src/app/components/LogoutButton.jsx
+++ 42gg/src/app/components/LogoutButton.jsx
@@ -1,9 +1,9 @@
 import { useResetRecoilState } from 'recoil';
-import { userState } from '../../atom';
+import { pageState, userState } from '../../atom';
 
 export default function LogoutButton() {
   const userReset = useResetRecoilState(userState);
-
+  const pageReset = useResetRecoilState(pageState);
   return (
     <button className='LogoutButton' onClick={userReset}>
       logout
