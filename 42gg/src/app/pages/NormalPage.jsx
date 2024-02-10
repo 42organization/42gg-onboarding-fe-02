e8974b157c336e1650d968f648a65b0ea0c82b72
conteng <contemplation.person@gmail.com>
2024-02-10 17:54:11 +0900

    feat: logout

--- 42gg/src/app/pages/NormalPage.jsx
+++ 42gg/src/app/pages/NormalPage.jsx
@@ -1,9 +1,19 @@
+import { useNavigate } from 'react-router-dom';
 import Navbar from '../components/NavBar.jsx';
 import Sidebar from '../components/Sidebar.jsx';
 import '../css/HomePage.css';
 import TodoPage from './TodoPage.jsx';
+import { useRecoilValue } from 'recoil';
+import { userState } from '../../atom.jsx';
+import { useEffect } from 'react';
 
 function NormalPage() {
+  const navigate = useNavigate();
+  const { role } = useRecoilValue(userState);
+  useEffect(() => {
+    if (!role) navigate('/');
+  }, [role]);
+
   return (
     <div className='admin-page'>
       <Navbar />
