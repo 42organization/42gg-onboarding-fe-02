acc6dc05984ec85da6e47300306fffc16d75dfd3
siwolee <siwolee@student.42seoul.kr>
2024-06-12 13:08:56 +0900

    test: component

--- fe02/src/pages/PartyRoom.tsx
+++ fe02/src/pages/PartyRoom.tsx
@@ -1,15 +1,15 @@
-import Image from 'next/image';
 import { Inter } from 'next/font/google';
+import Image from 'next/image';
 // import "../styles/PartyRoom.css"
 
 const inter = Inter({ subsets: ['latin'] });
 
 export default function PartyRoom() {
   return (
-    <main>
-      <div>
-        <h1>hello world!...?</h1>
-      </div>
-    </main>
+    <>
+      <header></header>
+      <main></main>
+      <footer></footer>
+    </>
   );
 }
