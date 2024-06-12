3a5dcd8cb109c7a1461943ba58db268dde02d387
Lee Jeong Ron <83465749+JeongRon@users.noreply.github.com>
2024-06-12 16:22:26 +0900

    Chore: 디렉토리 및 파일 정리, scss, tailwind 적용 체크

---
+++ fe02/src/pages/party/index.tsx
@@ -0,0 +1,12 @@
+import styles from '../../styles/party/PartyRoom.module.scss';
+
+const PartyDetailPage = () => {
+  return (
+    <div>
+      <h1 className='bg-sky-500'>hello world!...?</h1>
+      <button className={styles.tmpBtn}>Hello</button>
+    </div>
+  );
+};
+
+export default PartyDetailPage;
