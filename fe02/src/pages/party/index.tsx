65c837e96d08ccea7ab524237f39e1bd5ebccd82
Lee Jeong Ron <83465749+JeongRon@users.noreply.github.com>
2024-06-13 16:48:03 +0900

    Feat: PartyPage 구현

--- fe02/src/pages/party/index.tsx
+++ fe02/src/pages/party/index.tsx
@@ -1,12 +1,25 @@
-import styles from '@/styles/party/PartyRoom.module.scss';
+import Link from 'next/link';
+import type { NextPageWithLayout } from '../_app';
+import type { ReactElement } from 'react';
+import Layout from '@/components/Layout/Layout';
+import styles from '@/styles/party/Party.module.scss';
 
-const PartyDetailPage = () => {
+const PartyPage: NextPageWithLayout = () => {
   return (
-    <div>
-      <h1 className='bg-sky-500'>hello world!...?</h1>
-      <button className={styles.tmpBtn}>Hello</button>
+    <div className={styles.partyContainer}>
+      <h1>Party Page</h1>
+      <Link href='/party/1' className={styles.roomBtn}>
+        <div>파티 방 1번</div>
+      </Link>
+      <Link href='/party/2' className={styles.roomBtn}>
+        <div>파티 방 2번</div>
+      </Link>
     </div>
   );
 };
 
-export default PartyDetailPage;
+PartyPage.getLayout = function getLayout(page: ReactElement) {
+  return <Layout>{page}</Layout>;
+};
+
+export default PartyPage;
